b7f504fe444665739db7506dcd3f0eabe757202b
rvocampo26 <ocaruth1998@gmail.com>
2026-09-12 00:33:44 +0200

    pruebas de simulador + schemas

--- main.py
+++ main.py
@@ -1,10 +1,81 @@
+import argparse
 from datetime import datetime
 
 from src.simulator.simulator import Simulator
+def main():
+
+    parser = argparse.ArgumentParser(
+        description="Simulador logístico Kafka"
+    )
+
+    parser.add_argument(
+        "--steps",
+        type=int,
+        default=1,
+        help=(
+            "Número de horas simuladas "
+            "a ejecutar."
+        )
+    )
+
+    parser.add_argument(
+        "--realtime",
+        action="store_true",
+        help=(
+            "Espera 60 segundos reales "
+            "por cada hora simulada."
+        )
+    )
+
+    parser.add_argument(
+        "--reset",
+        action="store_true",
+        help=(
+            "Elimina el estado persistido "
+            "y crea una simulación nueva."
+        )
+    )
+
+    args = parser.parse_args()
+
+    # ---------------------------------------------------------
+    # FECHA INICIAL
+    # ---------------------------------------------------------
+    #
+    # Solo se utiliza si NO existe state.json.
+    #
+
+    fecha_actual = datetime(
+        2026,
+        9,
+        11,
+        6,
+        0,
+        0
+    )
+
+    simulator = Simulator(
+        fecha_actual
+    )
+
+    # ---------------------------------------------------------
+    # RESET
+    # ---------------------------------------------------------
+
+    if args.reset:
+
+        simulator.reset_state()
+
+    # ---------------------------------------------------------
+    # RUN
+    # ---------------------------------------------------------
+
+    simulator.run(
+        steps=args.steps,
+        realtime=args.realtime
+    )
 
 
-now = datetime(2026,9,9,6,0)
+if __name__ == "__main__":
 
-simulator = Simulator(fecha_actual=now)
-
-simulator.run()
+    main()
